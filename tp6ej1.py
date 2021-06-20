c71e403ab143068c697dd3a3d46d0a829d335d9f
Bruni <juanbruni@live.com>
2021-06-20 19:59:27 -0300

    Create tp6ej1.py

---
+++ tp6ej1.py
@@ -0,0 +1,37 @@
+################
+# Juan Ignacio Bruni - @Juanbruni
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+def anagrama(cadena1, cadena2):
+
+    palabra1 = list(cadena1.lower())
+    palabra2 = list(cadena2.lower())
+
+    palabra1.sort()
+    palabra2.sort()
+
+    if palabra1 == palabra2:
+        return True
+    else:
+        return False
+
+
+def principal():
+    texto1 = input('Ingrese palabra 1: ')
+    texto2 = input('Ingrese palabra 2: ')
+
+    cadena1 = texto1.replace(" ", "")
+    cadena2 = texto2.replace(" ", "")
+
+    resultado = anagrama(cadena1, cadena2)
+
+    if resultado:
+        print(f'{texto1} y {texto2} son un anagrama')
+    else:
+        print(f'{texto1} y {texto2} no son un anagrama')
+
+
+if __name__ == "__main__":
+    principal()
